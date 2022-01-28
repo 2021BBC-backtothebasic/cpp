9f7b8b6394c2bb9ba89c979d4ae3191861249c51
eumcloud <livelch012@naver.com>
2022-01-28 14:42:26 +0000

    modif

--- src/c0124/realloc1.cpp
+++ src/c0124/realloc1.cpp
@@ -2,58 +2,23 @@
 
 using namespace std;
 
-
-
 int main()
-
 {
-
 	    char cAlphabet, *pAlphabet, *ptr;
-
-	        pAlphabet = (char*)calloc(35, sizeof(char));
-
-		    
-
-		    if(pAlphabet == NULL)
-
-			        {
-
-					        cout << "Dynamic memory allocation fail..."<< endl;
-
-						        exit(1);
-
-							    }
-
-		        
-
-		        ptr = pAlphabet;
-
-			    
-
-			    for(cAlphabet = 0x41; cAlphabet < 0x5B; cAlphabet++)
-
-				        {
-
-						        *ptr++ = cAlphabet;
-
-							    }
-
-			        *ptr = '\0';
-
-				    
-
-				    cout << "Alphabet String : " << pAlphabet << endl;
-
-				        
-
-				        
-
-				        free(pAlphabet);
-
-					    
-
-					    return 0;
-
+        pAlphabet = (char*)calloc(35, sizeof(char));
+        if(pAlphabet == NULL)
+        {
+                cout << "Dynamic memory allocation fail..."<< endl;
+                exit(1);
+        }
+        ptr = pAlphabet;
+        for(cAlphabet = 0x41; cAlphabet < 0x5B; cAlphabet++)
+        {
+            *ptr++ = cAlphabet;
+            *ptr = '\0';
+            cout << "Alphabet String : " << pAlphabet << endl;
+            free(pAlphabet);
+            return 0;
 }
 
 
