8d0b222414b47e098fcd171dac6a6cead14e2f43
eumcloud <livelch012@naver.com>
2022-02-21 15:23:55 +0100

    thinking shell arr push for modif values

--- makeCoin/sedt1/echo.sh
+++ makeCoin/sedt1/echo.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+echo "작업명을 입력해주세요"
+read pjname
 
-find ./ -type f -readable -writable -exec sed -i 's/Litecoin/Dailycoin/g' {} \;
-find ./ -type f -readable -writable -exec sed -i 's/LiteCoin/DailyCoin/g' {} \;
+# splitpjname=($(echo $pjname | tr "" "\n"))
+# echo "whereis : ${splitpjname[0]} "
+
+arrpjname=$()
+for i in $pjname
+do
+	arrpjname+=(i)
+done
+echo $arrpjname[0]
+#read input
+#case $input in
+#	1) echo " first"
+#		echo " : 
+
+
+DECHO=$(find ./ -type f -readable -writable -exec sed -i 's/${pjname}coin/${pjname}coin/g' {} \;
+find ./ -type f -readable -writable -exec sed -i 's/DailyCoin/DailyCoin/g' {} \;
 find ./ -type f -readable -writable -exec sed -i 's/LTC/DLYC/g' {} \;
 find ./ -type f -readable -writable -exec sed -i 's/litecoin/dailycoin/g' {}\;
 find ./ -type f -readable -writable -exec sed -i 's/LITECOIN/DAILYCOIN/g' {} \;
-//find ./ -type f -readable -writable -exec sed -i 's/litecoind/dailycoind/g' {} \;
-
+# find ./ -type f -readable -writable -exec sed -i 's/litecoind/dailycoind/g' {} \;
+)
 
 #포트 변경
 find . -type f -print0 | xargs -0 sed -i 's/9333/9777/g'  {} \; # P2P port
