ef71a6bef4ef4dfac8fbdfb261e14dc6c99d7023
eumcloud <livelch012@naver.com>
2022-02-27 12:19:49 +0100

    Revert "source modif..2"
    This reverts commit 40952d90cea602f426486a71c006fbc0c595f02c.

--- makeCoin/customtool/test.cpp
+++
@@ -1,5 +0,0 @@
-#ifndef BITCOIN_CHAINP> ARAMSSEEDS_H
-#define BITCOIN_CHAINPARAMSSEEDS_H
-static SeedSpec6 pnSeed6_main[]={};
-static SeedSpec6 pnSeed6_test[] = {};
-#endif
--- makeCoin/customtool/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ints="20220226095056"
-
-echo $cvt
-cvt=$( date -d ${ints:4:2}/${ints:6:2}/${ints:0:4} ${ints:8:2}:${ints:10:2}:${ints:12:2} +"%s")
-
