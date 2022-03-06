e958813412c362c79356d76fff48c36624d3f52b
eumcloud <livelch012@naver.com>
2022-03-06 08:03:33 +0100

    text value belong '=' to variables in bash

--- makeCoin/customtool/getGensource/test/getline3.sh
+++ makeCoin/customtool/getGensource/test/getline3.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-gsource=`cat getSource.txt`
-
-OIFS=$IFS
 function getline {
-    IFS="="
-    
-    for x in $gsource
-    do
-        echo "$x"
-    done
+	gsource=`cat getSource.txt`
+	OIFS=$IFS
+	for i in $gsource
+	do
+		if [[ $i == *=* ]] ; then			
+			#  =을 포함한 줄은 j에 입력
+			IFS='=' read -ra j <<< $i
+		fi
+			#제네시스 해시값들 인스턴스
+		if [[ $j == *merkle_hash* ]] ; then merkle_hash=${j[1]}
+		fi
+		if [[ $j == *pszTimestamp* ]] ; then pszTimestamp=${j[1]}
+		fi
+		if [[ $j == *pubkey* ]] ; then pubkey=${j[1]}
+		fi
+		if [[ $j == *time* ]] ; then time=${j[1]}
+		fi
+		if [[ $j == *bits* ]] ; then bits=${j[1]}
+		fi
+		if [[ $j == *nonce* ]] ; then nonce=${j[1]}
+		fi
+		if [[ $j == *genesis_hash* ]] ; then genesis_hash=${j[1]}
+		fi
+	done
+	echo "merkle_hash :" $merkle_hash
+	IFS=$OIFS
+}
 
-}   
-
-IFS=$OIFS
 getline
+
 
